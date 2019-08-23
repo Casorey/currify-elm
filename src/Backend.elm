d3de0c7519b06bc317032f88b186c6ebf77666a0
Casorey <Casorey@hotmail.com.ar>
2019-08-22 23:43:35 -0300

    Update Backend.elm

--- src/Backend.elm
+++ src/Backend.elm
@@ -46,7 +46,7 @@ toggleLike : String -> List Song -> List Song
 toggleLike id songs = map (changeSongLike id) songs
 
 changeSongLike : String -> Song -> Song
-changeSongLike id song = if (findWithID id song).liked then dislike song else like song
+changeSongLike id songs song = if (findWithID id songs song).liked then dislike song else like song
 
 like : Song -> Song
 like song = {song |liked = True}
